977c639ccc0826db8964fe0834519d2a74f61fc6
Phantom <ramonadedotun550@gmail.com>
2025-07-20 23:21:06 +0100

    Task 3, Creating custom Views depending on user roles with HTML Template

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -6,6 +6,7 @@ from django.contrib.auth import authenticate
 from django.contrib.auth import login
 from django.contrib.auth import logout
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required, user_passes_test
 
 def register(request):
     if request.method == 'POST':
@@ -52,3 +53,31 @@ class LibraryDetailView(DetailView):
         context = super().get_context_data(**kwargs)
         context['books'] = self.object.books.all()
         return context
+
+
+def is_admin(user):
+    return user.is_authenticated and user.userprofile.role == 'admin'
+
+@login_required
+@user_passes_test(is_admin)
+
+def admin_dashboard(request):
+    return render(request, 'relationship_app/admin_view.html')
+
+def is_librarian(user):
+    return user.is_authenticated and user.userprofile.role == 'librarian'
+
+@login_required
+@user_passes_test(is_librarian)
+
+def librarian_Dashboard(request):
+    return render(request, 'relationship_app/librarian_view.html')
+
+def is_member(user):
+    return user.is_authenticated and user.userprofile.role == 'member'
+
+@login_required
+@user_passes_test(is_member)
+
+def member_dashboard(request):
+    return render(request, 'relationship_app/member_view.html')
