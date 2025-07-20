4ff9b10c42601d56587790fd1c8e743aa768f122
Phantom <ramonadedotun550@gmail.com>
2025-07-20 22:19:23 +0100

    Creating Login. Logout and Registration templates for the App

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -7,7 +7,7 @@ from django.contrib.auth import login
 from django.contrib.auth import logout
 from django.contrib.auth.forms import UserCreationForm
 
-def RegisterView(request):
+def register(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
