6fd063c369723666a38c6a805611b91ee16bf1ad
Phantom <ramonadedotun550@gmail.com>
2025-07-27 21:04:48 +0100

    Adding Custom Admin interface

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import Book
+from django.contrib.auth.admin import UserAdmin
 
 # Register your models here.
 @admin.register(Book)
@@ -10,3 +11,19 @@ class BookAdmin(admin.ModelAdmin):
 
 #list_display = ('title', 'author', 'publication_year')
 #list_filter = ('publication_year')
+
+
+class ModelAdmin (UserAdmin):
+    model = UserAdmin.model
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'date_of_birth')
+    list_filter = ('is_staff', 'is_superuser', 'is_active')
+    search_fields = ('username', 'email', 'is_staff', 'is_superuser', 'is_active')
+    ordering = ('username','email', 'first_name', 'last_name')
+
+admin.site.register(UserAdmin.model, ModelAdmin )
