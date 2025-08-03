742f36149bc3e048f8cace11275770966356c70b
Phantom <ramonadedotun550@gmail.com>
2025-08-03 02:51:26 +0100

    Importing DefaultRouters for the url.py and adding the BookList

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
 from .views import BookList
+from rest_framework.routers import DefaultRouter
+from .views import BookViewSet
+
+router = DefaultRouter()
+router.register(r'books_all', BookViewSet, basename='book_all')
 
 
 urlpatterns = [
