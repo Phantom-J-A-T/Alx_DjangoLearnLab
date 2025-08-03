a08a93c62eb803613768aad8fb016e66e1fe8e08
Phantom <ramonadedotun550@gmail.com>
2025-08-03 03:13:18 +0100

    Adding permissions to the Viewset

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -3,6 +3,8 @@ from .models import Book
 from rest_framework import generics
 from rest_framework import viewsets
 from .serializers import BookSerializer
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAdminUser
 
 # Create your views here.
 class BookList(generics.ListAPIView):
@@ -16,6 +18,20 @@ class BookViewSet(viewsets.ModelViewSet):
     """
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]  # Ensure the user is authenticated to access this viewset
+
+    def perform_create(self, serializer):
+        # Custom logic before saving a new book instance
+        serializer.save()
+
+class AdminViewSet(viewsets.ModelViewSet):
+    """
+    A viewset that provides the standard actions for the Book model,
+    accessible only to admin users.
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAdminUser]  # Ensure the user is authenticated to access this viewset
 
     def perform_create(self, serializer):
         # Custom logic before saving a new book instance
