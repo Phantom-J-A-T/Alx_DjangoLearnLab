9e27e7b1546c47df10f59b5602819a5267c66533
Phantom <ramonadedotun550@gmail.com>
2025-08-03 02:48:19 +0100

    Adding a BookViewSet to the View.py using Viewsets

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render
 from .models import Book
 from rest_framework import generics
+from rest_framework import viewsets
 from .serializers import BookSerializer
 
 # Create your views here.
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+
+
+class BookViewSet(viewsets.ModelViewSet):
+    """
+    A viewset that provides the standard actions for the Book model.
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+    def perform_create(self, serializer):
+        # Custom logic before saving a new book instance
+        serializer.save()
