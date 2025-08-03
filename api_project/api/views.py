6a8aac820c6e967527d4ff3120cabc42cbf3d5f3
Phantom <ramonadedotun550@gmail.com>
2025-08-03 02:42:37 +0100

    Creating a Serializer.py and adding a Book component to the MOdel.py, adding a BookList to the Views.py also creating a urls.py

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Book
+from rest_framework import generics
+from .serializers import BookSerializer
 
 # Create your views here.
+class BookList(generics.ListCreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
