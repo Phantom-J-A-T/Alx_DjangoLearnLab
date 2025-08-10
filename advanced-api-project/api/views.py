d42eb8056bd36e4f40d135708c629d17637f8193
Phantom <ramonadedotun550@gmail.com>
2025-08-10 03:03:32 +0100

    Creating a Ordering filter to Order for books by title and publication year

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -7,6 +7,7 @@ from .models import Book
 from .serializers import BookSerializer
 from .filters import BookFilter
 from django_filters.rest_framework import DjangoFilterBackend
+#from django_filters import rest_framework
 
 # Create your views here.
  # Implememting Generic views for the Book model   
