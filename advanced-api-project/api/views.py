c9be436b3de66f46ff8e89ab0a3af6b6ed529744
Phantom <ramonadedotun550@gmail.com>
2025-08-10 02:41:29 +0100

    Changing the permission from AllowAny to IsAuthenticatedorReadOnly

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from rest_framework import generics
 from rest_framework import permissions
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 from .models import Book
 from .serializers import BookSerializer
 
@@ -9,12 +10,12 @@ from .serializers import BookSerializer
 class ListView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = [permissions.AllowAny]  # allow unauthenticated users to read, but authenticated users to write
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]  # allow unauthenticated users to read, but authenticated users to write
 
 class DetailView(generics.RetrieveAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = [permissions.AllowAny]  # allow unauthenticated users to read, but authenticated users to write
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]  # allow unauthenticated users to read, but authenticated users to write
 
 class CreateView(generics.CreateAPIView):
     queryset = Book.objects.all()
