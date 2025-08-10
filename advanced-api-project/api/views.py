e913d5a9b8397f4dc0442621bc007f56aebef2af
Phantom <ramonadedotun550@gmail.com>
2025-08-10 02:31:46 +0100

    Including permissions to the Createand Update views

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from rest_framework import generics
+from rest_framework import permissions
 from .models import Book
 from .serializers import BookSerializer
 
@@ -16,10 +17,20 @@ class DetailView(generics.RetrieveAPIView):
 class CreateView(generics.CreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]  # restrict creation to authenticated users
+
+    def perform_create(self, serializer):
+        # Giving the authnticated user the ability to create a book and save it
+        serializer.save()
 
 class UpdateView(generics.UpdateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]  # restrict updates to authenticated users
+
+    def perform_update(self, serializer):
+        # Giving the authenticated user the ability to update a book and save it
+        serializer.save()
 
 class DeleteView(generics.DestroyAPIView):
     queryset = Book.objects.all()
