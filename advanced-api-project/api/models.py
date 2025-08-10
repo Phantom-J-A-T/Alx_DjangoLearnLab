1ac46da54a54971ab375df19aa6b35a0256dae32
Phantom <ramonadedotun550@gmail.com>
2025-08-10 02:22:35 +0100

    Setting up Generic views in the Models for the Book model

--- advanced-api-project/api/models.py
+++ advanced-api-project/api/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from rest_framework import generics
+from .serializers import BookSerializer
 
 # Create your models here.
 class Author(models.Model):
@@ -15,3 +17,26 @@ class Book(models.Model):
 
     def __str__(self):
         return self.title
+    
+ # Implememting Generic views for the Book model   
+class ListView(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class DetailView(generics.RetrieveAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class CreateView(generics.CreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class UpdateView(generics.UpdateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class DeleteView(generics.DestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+    
