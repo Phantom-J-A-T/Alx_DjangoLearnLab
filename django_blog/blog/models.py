92160c7039f8fb04da65cf464ee5a2dd432f8151
Phantom <ramonadedotun550@gmail.com>
2025-08-12 01:19:40 +0100

    Creating a Template folder and adding a Base, Styles, and Script

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class blog(models.Model):
+    title = models.CharField(max_length=200, verbose_name="标题")
+    content = models.TextField(verbose_name="内容")
+    published_date = models.DateTimeField(auto_now_add=True, verbose_name="发布日期")
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE, verbose_name="作者")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="创建时间")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="更新时间")
+
+    class Meta:
+        verbose_name = "博客"
+        verbose_name_plural = "博客"
+
+    def __str__(self):
+        return self.title
--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -52,10 +52,16 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'django_blog.urls'
 
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],  # Directory for custom templates
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
