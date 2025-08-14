45cd614f827300076a23aa98e75c4ab668b9ec2b
Phantom <ramonadedotun550@gmail.com>
2025-08-14 15:39:20 +0100

    Revert blog folder to version from commit 0311d5d

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,13 +1,20 @@
-from django.contrib import admin
-from django.urls import path
+from . import views
 from django.contrib.auth import views as auth_views
-from blog import views  # your own app views
+from django.urls import path
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('admin/', admin.site.urls),
+    # Home page
+    path('', views.index, name='index'),
+    
+    # Blog post detail view
+    path('post/<int:post_id>/', views.post_detail, name='post_detail'),
+    
+    # User registration
     path('register/', views.register, name='register'),
+    
+    # User login
     path('login/', auth_views.LoginView.as_view(template_name='blog/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
-    path('profile/', views.profile, name='profile'),
+    
+    # User logout
+    path('logout/', auth_views.LogoutView.as_view(template_name = 'blog/logout.html'), name='logout'),
 ]
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -21,23 +21,3 @@ def register(request):
     else:
         form = CustomUserCreationForm()
     return render(request, 'blog/register.html', {'form': form})
-
-def home(request):
-    return render(request, 'home.html')
-
-
-from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login
-from django.contrib import messages
-
-def login_view(request):
-    if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            return redirect('profile')
-        else:
-            messages.error(request, 'Invalid username or password')
-    return render(request, 'login.html')
