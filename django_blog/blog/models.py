ec6cda9c48d9f3122ad059466e4bbdbf69f5b32c
Phantom <ramonadedotun550@gmail.com>
2025-08-14 17:48:03 +0100

    Creating the Comment Model

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -30,3 +30,17 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return reverse('post_detail', args=[str(self.id)])
+    
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'Comment by {self.author.username} on {self.post.title}'
+    
+    def get_absolute_url(self):
+        return reverse('post_detail', args=[str(self.post.id)])
