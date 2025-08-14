4f8a4379d6c253126867f784a95717fbd48485b0
Phantom <ramonadedotun550@gmail.com>
2025-08-14 03:19:31 +0100

    Checker Issues

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('admin/', admin.site.urls),
     path('register/', views.register, name='register'),
-    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('login/', auth_views.LoginView.as_view(template_name='blog/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
     path('profile/', views.profile, name='profile'),
 ]
