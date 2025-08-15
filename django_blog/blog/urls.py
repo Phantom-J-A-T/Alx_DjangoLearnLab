c50347433d72f8f59e6280cb4a3cf27acb069536
Phantom <ramonadedotun550@gmail.com>
2025-08-15 01:53:48 +0100

    Updating the urls a

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -41,5 +41,5 @@ urlpatterns = [
     path('comment/<int:pk>/update/', CommentUpdateView.as_view(), name='comment_update'),
     path('comment/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment_delete'),
     path('search/', views.search_posts, name='search-posts'),
-    path('tag/<slug:slug>/', views.posts_by_tag, name='posts-by-tag'),
+    path('tags/<slug:tag_slug>/', views.PostByTagListView.as_view(), name='posts-by-tag'),
 ]
