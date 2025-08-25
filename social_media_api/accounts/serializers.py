c7636950a35413e860f099040b6224964b5ce594
Phantom <ramonadedotun550@gmail.com>
2025-08-26 00:58:32 +0100

    UPdating the Serializer

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -11,14 +11,14 @@ class UserRegisterSerializer(serializers.ModelSerializer):
         fields = ['id', 'username', 'email', 'password', 'bio', 'profile_picture']
 
     def create(self, validated_data):
-        user = CustomUser.objects.create_user(
+        # use create_user so password is hashed
+        return CustomUser.objects.create_user(
             username=validated_data['username'],
             email=validated_data.get('email'),
             password=validated_data['password'],
             bio=validated_data.get('bio', ''),
             profile_picture=validated_data.get('profile_picture', None)
         )
-        return user
 
 
 # Login Serializer
