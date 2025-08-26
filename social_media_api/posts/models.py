1b74d57fd5c4d7f5f00c4823c158c9c279535c71
Phantom <ramonadedotun550@gmail.com>
2025-08-26 01:10:53 +0100

    Creating a Post and Comment Models

--- social_media_api/posts/models.py
+++ social_media_api/posts/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    author = models.CharField(max_length=100)
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+    
+class Comment(models.Model):
+    author = models.ForeignKey(Post, related_name='post_comments', on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'Comment on {self.post.title}'
