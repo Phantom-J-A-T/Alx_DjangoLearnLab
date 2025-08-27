9432f7227d5b8331280352d014a96b287b3b5092
Phantom <ramonadedotun550@gmail.com>
2025-08-27 20:58:45 +0100

    Creating aNotification app

--- social_media_api/posts/models.py
+++ social_media_api/posts/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
 class Post(models.Model):
@@ -20,3 +21,14 @@ class Comment(models.Model):
 
     def __str__(self):
         return f'Comment on {self.post.title}'
+
+class Like(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="likes")
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="likes")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ("user", "post")  # prevents duplicate likes
+
+    def __str__(self):
+        return f"{self.user.username} liked {self.post.title}"
