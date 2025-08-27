645482cc585992d37e186030153d4b7d6a3b4ee5
Phantom <ramonadedotun550@gmail.com>
2025-08-27 20:14:23 +0100

    Creating the Followers model and adding it to the Custom users

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -1,11 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
 class CustomUser(AbstractUser):
     bio = models.TextField(blank=True, null=True)
     profile_picture = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
-    followers = models.ManyToManyField('self', symmetrical=False, related_name='following', blank=True)
+    
+    # Only one field needed
+    following = models.ManyToManyField(
+        'self',
+        symmetrical=False,   # following ≠ followers
+        related_name='followers',  
+        blank=True
+    )
 
     def __str__(self):
         return self.username
