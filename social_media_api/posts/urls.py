b5d1f5e0cbed625a91f984dc61f0e6605d4913fc
Phantom <ramonadedotun550@gmail.com>
2025-08-27 20:44:04 +0100

    Updating the Views, Serializers and url for the Post

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -10,3 +10,7 @@ urlpatterns = [
     path('', include(router.urls)),
     path('feed/', FeedView.as_view(), name='user-feed'),
 ]
+
+
+# path("follow/<int:pk>/", views.CustomUserViewSet.as_view({"post": "follow"}), name="follow-user"),
+#   path("unfollow/<int:pk>/", views.CustomUserViewSet.as_view({"post": "unfollow"}), name="unfollow-user"),
